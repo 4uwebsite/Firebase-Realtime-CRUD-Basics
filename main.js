1940c58bbf5b9de81486a27370fd7edc38fdd59f
4uwebsite <ramee.nana@gmail.com>
2024-09-27 13:18:29 +0530

    0.0.0

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"
+
 const appSettings = {
     databaseURL: 'https://crud-basics-e2bb0-default-rtdb.asia-southeast1.firebasedatabase.app/'
 }
